5a9d4ac0c96d5b51c1f0b5807244953b0d8325c2
Michael Büchner <m.buechner@dnb.de>
2020-04-27 21:54:50 +0200

    Url encoding in file-overview.vue

--- src/main/resources/vue/file-overview.vue
+++ src/main/resources/vue/file-overview.vue
@@ -2,9 +2,9 @@
     <div aria-orientation="vertical" class="nav flex-column nav-pills" id="v-pills-tab" role="tablist">
         <a class="nav-link d-none" href="#" id="directoryup" title="Übergeordnetes Verzeichnis">..</a>
         <template v-for="file in data">
-            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.filenameWithPath}`" class="nav-link" title="Verzeichnis" v-if="file.type === 'directory'">{{file.filename}}/</a>
-            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.path}&amp;f=${file.filenameWithPath}`" class="nav-link active" title="Ausgewählte Datei" v-else-if="file.type === 'file' && file.filenameWithPath === f">{{file.filename}}</a>
-            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.path}&amp;f=${file.filenameWithPath}`" class="nav-link" title="Datei" v-else-if="file.type === 'file'">{{file.filename}}</a>
+            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${encodeURIComponent(file.filenameWithPath)}`" class="nav-link" title="Verzeichnis" v-if="file.type === 'directory'">{{file.filename}}/</a>
+            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${encodeURIComponent(file.path)}&amp;f=${encodeURIComponent(file.filenameWithPath)}`" class="nav-link active" title="Ausgewählte Datei" v-else-if="file.type === 'file' && encodeURIComponent(file.filenameWithPath) === f">{{file.filename}}</a>
+            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${encodeURIComponent(file.path)}&amp;f=${encodeURIComponent(file.filenameWithPath)}`" class="nav-link" title="Datei" v-else-if="file.type === 'file'">{{file.filename}}</a>
         </template>
     </div>
 </template>
@@ -23,11 +23,11 @@
             let jscript = document.createElement('script');
             jscript.setAttribute('src', Vue.prototype.$javalin.state.baseurl + '/ddblabs-iiif-presentation.min.js');
             document.body.appendChild(jscript);
-            fetch(Vue.prototype.$javalin.state.baseurl + "/api/browse?d=" + (this.$route.query.d ? this.$route.query.d : ""))
+            fetch(Vue.prototype.$javalin.state.baseurl + "/api/browse?d=" + (this.$route.query.d ? encodeURIComponent(this.$route.query.d) : ""))
                 .then(res => res.json())
                 .then(res => this.data = res)
                 .catch(() => alert("Error while fetching files"));
-            this.f = this.$route.query.f;
+            this.f = encodeURIComponent(this.$route.query.f);
         },
     });
 </script>
