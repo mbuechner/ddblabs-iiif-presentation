ee5db9697b4bac77b7106e48d43793a40bf0c064
Michael Büchner <m.buechner@dnb.de>
2020-04-27 10:53:39 +0200

    ...

--- src/main/resources/vue/file-overview.vue
+++ src/main/resources/vue/file-overview.vue
@@ -2,13 +2,9 @@
     <div aria-orientation="vertical" class="nav flex-column nav-pills" id="v-pills-tab" role="tablist">
         <a class="nav-link d-none" href="#" id="directoryup" title="Übergeordnetes Verzeichnis">..</a>
         <template v-for="file in data">
-            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.filenameWithPath}`" class="nav-link"
-               title="Verzeichnis" v-if="file.type === 'directory'">{{file.filename}}/</a>
-            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.path}&f=${file.filenameWithPath}`"
-               class="nav-link active" title="Ausgewählte Datei"
-               v-else-if="file.type === 'file' && file.filenameWithPath === f">{{file.filename}}</a>
-            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.path}&f=${file.filenameWithPath}`"
-               class="nav-link" title="Datei" v-else-if="file.type === 'file'">{{file.filename}}</a>
+            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.filenameWithPath}`" class="nav-link" title="Verzeichnis" v-if="file.type === 'directory'">{{file.filename}}/</a>
+            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.path}&f=${file.filenameWithPath}`" class="nav-link active" title="Ausgewählte Datei" v-else-if="file.type === 'file' && file.filenameWithPath === f">{{file.filename}}</a>
+            <a :href="`${Vue.prototype.$javalin.state.baseurl}?d=${file.path}&f=${file.filenameWithPath}`" class="nav-link" title="Datei" v-else-if="file.type === 'file'">{{file.filename}}</a>
         </template>
     </div>
 </template>
@@ -24,6 +20,9 @@
             f: "",
         }),
         created() {
+            let jscript = document.createElement('script');
+            jscript.setAttribute('src', Vue.prototype.$javalin.state.baseurl + '/ddblabs-iiif-presentation.min.js');
+            document.body.appendChild(jscript);
             fetch(Vue.prototype.$javalin.state.baseurl + "/api/browse?d=" + (this.$route.query.d ? this.$route.query.d : ""))
                 .then(res => res.json())
                 .then(res => this.data = res)
