27c01144fb3d67875f8f7af3d618b1203eea4443
Michael Büchner <m.buechner@dnb.de>
2025-04-11 20:48:52 +0200

    Added DDBviewer

---
+++ src/main/resources/viewer/ddbviewer/860.main.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkddbviewer=self.webpackChunkddbviewer||[]).push([[860],{39803:(e,n,r)=>{r.r(n),r.d(n,{IconSvgPaths16:()=>c,IconSvgPaths20:()=>a,getIconPaths:()=>o,iconNameToPathsRecordKey:()=>u});var t=r(43261),c=r(75495),a=r(15312),s=r(6134);function o(e,n){var r=(0,t.fL)(e);return n===s.l.STANDARD?c[r]:a[r]}function u(e){return(0,t.fL)(e)}}}]);
